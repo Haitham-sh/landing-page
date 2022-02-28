85dfc6c015f0f28df80b19b14387b44bbc63193d
HAITHAM MOHAMED MOHAMED ELSHERBINY <91070657+Haitham-sh@users.noreply.github.com>
2022-02-28 23:37:42 +0200

    add go to top button

--- js/app.js
+++ js/app.js
@@ -1,76 +1,85 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-const all =document.querySelectorAll("section");
-const thePar =document.getElementById("navbar__list");
-const links =document.createElement('li');
+ *
+ */
+const all = document.querySelectorAll("section");
+const thePar = document.getElementById("navbar__list");
+const links = document.createElement("li");
 
 /**
  * End Global Variables
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-// Build menu 
+// Build menu
 // Scroll to anchor ID
-for(let i = 0; i < all.length; i++){
-  const call = all[i].getAttribute('data-nav');
-  const sete = all[i].getAttribute('id');
-  const strig ='<a class="menu__link" data-nav='+sete+' href="#'+sete+'">'+call+'</a>';
-  const link = document.createElement('li');
+for (let i = 0; i < all.length; i++) {
+  const call = all[i].getAttribute("data-nav");
+  const sete = all[i].getAttribute("id");
+  const strig =
+    '<a class="menu__link" data-nav=' +
+    sete +
+    ' href="#' +
+    sete +
+    '">' +
+    call +
+    "</a>";
+  const link = document.createElement("li");
   link.innerHTML = strig;
-  link.classList.add(sete)
+  link.classList.add(sete);
   thePar.appendChild(link);
-};
-
+}
 
 /**
  * End Main Functions
  * Add class 'active' to section when near top of viewport
  * Begin Events
- * 
-*/
-const elemanta = document.querySelectorAll('a')
-window.addEventListener('scroll',function(){
-const activity = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("your-active-class");
-    }
-    else {
-      entry.target.classList.remove("your-active-class");
-      } 
-  })
-},{ threshold:0.6})
+ *
+ */
+const elemanta = document.querySelectorAll("a");
+window.addEventListener("scroll", function () {
+  const activity = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("your-active-class");
+        } else {
+          entry.target.classList.remove("your-active-class");
+        }
+      });
+    },
+    { threshold: 0.6 }
+  );
   all.forEach((section) => {
-  activity.observe(section);
-})
-})
+    activity.observe(section);
+  });
+});
 
 // Scroll to section on link click
-const lis =document.querySelectorAll("li");
-for(let x = 0; x < lis.length; x++){
-lis[x].addEventListener('click', function (event) { 
-  event.preventDefault();
-  all[x].scrollIntoView({behavior: "smooth"});
-})};
+const lis = document.querySelectorAll("li");
+for (let x = 0; x < lis.length; x++) {
+  lis[x].addEventListener("click", function (event) {
+    event.preventDefault();
+    all[x].scrollIntoView({ behavior: "smooth" });
+  });
+}
 
 // Set sections as active
 window.addEventListener("scroll", () => {
@@ -82,20 +91,34 @@ window.addEventListener("scroll", () => {
       fay = section.getAttribute("id");
     }
   });
-    lis.forEach((li) => { 
-        let chld = li.firstChild
-      if (li.classList.contains(fay)) {
-          chld.classList.add("toto");
-          chld.classList.remove("menu__link");
+  lis.forEach((li) => {
+    let chld = li.firstChild;
+    if (li.classList.contains(fay)) {
+      chld.classList.add("toto");
+      chld.classList.remove("menu__link");
+    } else {
+      chld.classList.remove("toto");
+      chld.classList.add("menu__link");
     }
-    else {
-          chld.classList.remove("toto");
-          chld.classList.add("menu__link");
-    } 
   });
 });
 
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
